bd3054611db19dfa71a1720de79c6ffcf01e99a2
Martin Lehmann <https://github.com/MM-Lehmann>
2023-12-16 16:43:33 +0100

    save slides as jpg, not png

--- pptx2h5p.py
+++ pptx2h5p.py
@@ -3,7 +3,7 @@ import os
 import sys
 import uuid
 from copy import deepcopy
-from zipfile import ZipFile
+from zipfile import ZipFile, ZIP_DEFLATED
 
 from natsort import natsorted
 from win32com import client
@@ -33,59 +33,63 @@ def ppt2png(file):
 
 
 def add_to_json(newfile, image_folder, images, title):
-    exclude_files = ['content/content.json', 'h5p.json']
+    reserved_files = ["content\content.json", ".\h5p.json"]
     if getattr(sys, 'frozen', False):
         basedir = sys._MEIPASS
     else:
         basedir = os.path.dirname(os.path.abspath(__file__))
-    template = os.path.join(basedir, "template.h5p")
-    
+    template = os.path.join(basedir, "template")
+
     img_width, img_height = get_image_size(os.path.join(image_folder, images[0]))
 
-    with ZipFile(template, "r") as zin:
-        with ZipFile(newfile, "w") as zout:
-            # copy all other files
-            for item in zin.infolist():
-                if item.filename not in exclude_files:
-                    zout.writestr(item, zin.read(item.filename))
+    with ZipFile(newfile, "w", compression=ZIP_DEFLATED) as zout:
+        # add all other files to zip
+        for dir_, _, files in os.walk(template):
+            for file in files:
+                rel_dir = os.path.relpath(dir_, template)
+                rel_file = os.path.join(rel_dir, file)
+                abs_file = os.path.join(dir_, file)
+                if rel_file in reserved_files:
+                    continue
+                zout.write(abs_file, rel_file)
 
-            # add image filenames to content.json
-            with zin.open("content/content.json") as fp:
-                content = json.load(fp)
-                print(f"adding images from {image_folder}: {images}")
-                for i, image in enumerate(images):
-                    slides = content["presentation"]["slides"]
-                    # clone first element
-                    if i > 0 and len(slides) <= i:
-                        slides.append(deepcopy(slides[0]))
-                    elements = slides[i]["elements"][0]
-                    params = elements["action"]["params"]
-                    # add new image filename
-                    params["file"]["path"] = "images/" + image
-                    # add random uuid
-                    elements["action"]["subContentId"] = str(uuid.uuid4())
-                    # set width & height
-                    params["file"]["width"] = img_width
-                    params["file"]["height"] = img_height
-                    ratio = img_width / img_height
-                    if ratio > target_ratio:  # wider, need to shrink y
-                        elements["y"] = 100 * (1 - target_ratio / ratio) / 2
-                        elements["height"] = 100 * target_ratio / ratio
-                    elif ratio < target_ratio:  # higher, need to shrink x
-                        elements["x"] = 100 * (1 - ratio / target_ratio) / 2
-                        elements["width"] = 100 * ratio / target_ratio
-            # save file
-            zout.writestr("content/content.json", json.dumps(content))
+        # add image filenames to content.json
+        with open(template + "/content/content.json") as fp:
+            content = json.load(fp)
+        print(f"adding images from::\n\t {image_folder} {images}")
+        for i, image in enumerate(images):
+            slides = content["presentation"]["slides"]
+            # clone first element
+            if i > 0 and len(slides) <= i:
+                slides.append(deepcopy(slides[0]))
+            elements = slides[i]["elements"][0]
+            params = elements["action"]["params"]
+            # add new image filename
+            params["file"]["path"] = "images/" + image
+            # add random uuid
+            elements["action"]["subContentId"] = str(uuid.uuid4())
+            # set width & height
+            params["file"]["width"] = img_width
+            params["file"]["height"] = img_height
+            ratio = img_width / img_height
+            if ratio > target_ratio:  # wider, need to shrink y
+                elements["y"] = 100 * (1 - target_ratio / ratio) / 2
+                elements["height"] = 100 * target_ratio / ratio
+            elif ratio < target_ratio:  # higher, need to shrink x
+                elements["x"] = 100 * (1 - ratio / target_ratio) / 2
+                elements["width"] = 100 * ratio / target_ratio
+        # save file
+        zout.writestr("content/content.json", json.dumps(content))
 
-            # add image files to tip
-            for image in images:
-                zout.write(os.path.join(image_folder, image), "content/images/" + image)
+        # add image files to tip
+        for image in images:
+            zout.write(os.path.join(image_folder, image), "content/images/" + image)
 
-            # change presentation title
-            with zin.open("h5p.json", "r") as h5p:
-                content = json.load(h5p)
-                content["title"] = title
-            zout.writestr("h5p.json", json.dumps(content))
+        # change presentation title
+        with open(template + "/h5p.json", "r") as h5p:
+            content = json.load(h5p)
+            content["title"] = title
+        zout.writestr("h5p.json", json.dumps(content))
 
 
 if __name__ == "__main__":
